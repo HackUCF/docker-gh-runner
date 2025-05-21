ae0c5283c4fd63e550d4c614c9b711eb91ba515f
Jonathan Styles <64489881+jontyms@users.noreply.github.com>
2025-05-20 21:28:19 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,9 @@
 ORGANIZATION=$ORGANIZATION
 ACCESS_TOKEN=$ACCESS_TOKEN
 
+[[ -z "${ACCESS_TOKEN:-}"  ]] && { echo "ACCESS_TOKEN is required";  exit 1; }
+[[ -z "${ORGANIZATION:-}" ]] && { echo "ORGANIZATION is required"; exit 1; }
+
 REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
